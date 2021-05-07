9404d8cd01d36de66d27156b0733dda1f863e63e
Amilton Leme <amilton.leme@gmail.com>
2021-05-07 18:24:51 -0300

    Basic static components

---
+++ frontend/src/components/Fotter/index.tsx
@@ -0,0 +1,13 @@
+const Fotter = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/amiltonleme" target="_blank" rel="noreferrer">Amilton Leme</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Fotter;
