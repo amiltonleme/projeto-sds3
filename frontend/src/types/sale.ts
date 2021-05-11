ce33f8393afd8a2875c85b9898acb8557b283ed7
Amilton Leme <amilton.leme@gmail.com>
2021-05-10 22:44:52 -0300

    DataTable Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,32 @@
-export type saleSum = {
+
+import { Seller } from "./seller"
+
+export type Sale = {
+
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    //elements depois pages
+    size?: number;
+    number: number;
+
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+
+export type SaleSum = {
     sellerName: string;
     sum: number;
 }
