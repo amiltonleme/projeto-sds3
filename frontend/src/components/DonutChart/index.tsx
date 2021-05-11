ed443da48deda022d2df998a84c80bcd2094fc30
Amilton Leme <amilton.leme@gmail.com>
2021-05-10 22:01:06 -0300

    BarChat and DonutChat Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { saleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -11,18 +12,21 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //forma errada
-    let chartData: ChartData = { labels: [], series: [] };
-    //forma errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as saleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData);
-        });
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as saleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+
+    }, [])
 
     const options = {
         legend: {
